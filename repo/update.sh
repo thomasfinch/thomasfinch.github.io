ab694b4d27363e05c69668ffbdb85e9e2b661c9e
thomasfinch <tomf64@gmail.com>
2014-11-07 01:18:45 -0500

    fix things?

--- repo/update.sh
+++ repo/update.sh
@@ -3,14 +3,14 @@ dpkg-scanpackages -m . /dev/null > Packages
 bzip2 -f -k Packages
 
 #Get MD5 hashes of Packages and Packages.bz2, and put that info at the end of the release file
-PACKAGES_SIZE=$(stat -f%z Packages)
-PACKAGES_BZ2_SIZE=$(stat -f%z Packages.bz2)
-PACKAGES_MD5=$(md5 -q Packages)
-PACKAGES_BZ2_MD5=$(md5 -q Packages.bz2)
-cat Release-orig > Release
-printf "MD5Sum:\n %s %s %s" $PACKAGES_MD5 $PACKAGES_SIZE "Packages" >> Release
-printf "\n %s %s %s" $PACKAGES_BZ2_MD5 $PACKAGES_BZ2_SIZE "Packages.bz2" >> Release
+# PACKAGES_SIZE=$(stat -f%z Packages)
+# PACKAGES_BZ2_SIZE=$(stat -f%z Packages.bz2)
+# PACKAGES_MD5=$(md5 -q Packages)
+# PACKAGES_BZ2_MD5=$(md5 -q Packages.bz2)
+# cat Release-orig > Release
+# printf "MD5Sum:\n %s %s %s" $PACKAGES_MD5 $PACKAGES_SIZE "Packages" >> Release
+# printf "\n %s %s %s" $PACKAGES_BZ2_MD5 $PACKAGES_BZ2_SIZE "Packages.bz2" >> Release
 
 #Sign the release file
-rm -f Release.gpg
-gpg -abs -o Release.gpg Release
+# rm -f Release.gpg
+# gpg -abs -o Release.gpg Release
