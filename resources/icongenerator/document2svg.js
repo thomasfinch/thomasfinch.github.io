ce838883b30291867c4d5962a3d4a3a81ab46205
Thomas Finch <tomf64@gmail.com>
2015-06-29 13:13:01 -0400

    added html rasterizer

---
+++ resources/icongenerator/document2svg.js
@@ -0,0 +1,112 @@
+var document2svg = (function (util, browser, documentHelper, mediaQueryHelper, xmlserializer) {
+    "use strict";
+
+    var module = {};
+
+    var zoomedElementSizingAttributes = function (size, zoomFactor) {
+        var closestScaledWith, closestScaledHeight,
+            offsetX, offsetY;
+
+        zoomFactor = zoomFactor || 1;
+        closestScaledWith = Math.round(size.viewportWidth);
+        closestScaledHeight = Math.round(size.viewportHeight);
+
+        offsetX = -size.left;
+        offsetY = -size.top;
+
+        var attributes = {
+             'x': offsetX,
+             'y': offsetY,
+             'width': closestScaledWith,
+             'height': closestScaledHeight
+        };
+
+        if (zoomFactor !== 1) {
+            attributes.transform = 'scale(' + zoomFactor + ')';
+        }
+
+        return attributes;
+    };
+
+    var workAroundCollapsingMarginsAcrossSVGElementInWebKitLike = function (attributes) {
+        var style = attributes.style || '';
+        attributes.style = style + 'float: left;';
+    };
+
+    var workAroundSafariSometimesNotShowingExternalResources = function (attributes) {
+        /* Let's hope that works some magic. The spec says SVGLoad only fires
+         * now when all externals are available.
+         * http://www.w3.org/TR/SVG/struct.html#ExternalResourcesRequired */
+        attributes.externalResourcesRequired = true;
+    };
+
+    var workAroundChromeShowingScrollbarsUnderLinuxIfHtmlIsOverflowScroll = function () {
+        return '<style scoped="">html::-webkit-scrollbar { display: none; }</style>';
+    };
+
+    var serializeAttributes = function (attributes) {
+        var keys = Object.keys(attributes);
+        if (!keys.length) {
+            return '';
+        }
+
+        return ' ' + keys.map(function (key) {
+            return key + '="' + attributes[key] + '"';
+        }).join(' ');
+    };
+
+    var convertDocumentToSvg = function (doc, size, zoomFactor) {
+        var xhtml = xmlserializer.serializeToString(doc);
+
+        browser.validateXHTML(xhtml);
+
+        var attributes = zoomedElementSizingAttributes(size, zoomFactor);
+
+        workAroundCollapsingMarginsAcrossSVGElementInWebKitLike(attributes);
+        workAroundSafariSometimesNotShowingExternalResources(attributes);
+
+        return (
+            '<svg xmlns="http://www.w3.org/2000/svg"' +
+                ' width="' + size.width + '"' +
+                ' height="' + size.height + '"' +
+                ' font-size="' + size.rootFontSize + '"' +
+                '>' +
+                workAroundChromeShowingScrollbarsUnderLinuxIfHtmlIsOverflowScroll() +
+                '<foreignObject' + serializeAttributes(attributes) + '>' +
+                xhtml +
+                '</foreignObject>' +
+                '</svg>'
+        );
+    };
+
+    module.getSvgForDocument = function (doc, size, zoomFactor) {
+        documentHelper.rewriteTagNameSelectorsToLowerCase(doc);
+
+        return mediaQueryHelper.needsEmWorkaround().then(function (needsWorkaround) {
+            if (needsWorkaround) {
+                mediaQueryHelper.workAroundWebKitEmSizeIssue(doc);
+            }
+
+            return convertDocumentToSvg(doc, size, zoomFactor);
+        });
+    };
+
+    module.drawDocumentAsSvg = function (doc, options) {
+        if (options.hover) {
+            documentHelper.fakeHover(doc, options.hover);
+        }
+        if (options.active) {
+            documentHelper.fakeActive(doc, options.active);
+        }
+        if (options.focus) {
+            documentHelper.fakeFocus(doc, options.focus);
+        }
+
+        return browser.calculateDocumentContentSize(doc, options)
+            .then(function (size) {
+                return module.getSvgForDocument(doc, size, options.zoom);
+            });
+    };
+
+    return module;
+}(util, browser, documentHelper, mediaQueryHelper, xmlserializer));
---
+++ resources/icongenerator/documentHelper.js
@@ -0,0 +1,75 @@
+var documentHelper = (function (documentUtil) {
+    "use strict";
+
+    var module = {};
+
+    var asArray = function (arrayLike) {
+        return Array.prototype.slice.call(arrayLike);
+    };
+
+    module.fakeHover = function (doc, hoverSelector) {
+        var elem = doc.querySelector(hoverSelector),
+            fakeHoverClass = 'rasterizehtmlhover';
+        if (! elem) {
+            return;
+        }
+
+        documentUtil.addClassNameRecursively(elem, fakeHoverClass);
+        documentUtil.rewriteCssSelectorWith(doc, ':hover', '.' + fakeHoverClass);
+    };
+
+    module.fakeActive = function (doc, activeSelector) {
+        var elem = doc.querySelector(activeSelector),
+            fakeActiveClass = 'rasterizehtmlactive';
+        if (! elem) {
+            return;
+        }
+
+        documentUtil.addClassNameRecursively(elem, fakeActiveClass);
+        documentUtil.rewriteCssSelectorWith(doc, ':active', '.' + fakeActiveClass);
+    };
+
+    module.fakeFocus = function (doc, focusSelector) {
+        var elem = doc.querySelector(focusSelector),
+            fakeFocusClass = 'rasterizehtmlfocus';
+        if (! elem) {
+            return;
+        }
+
+        documentUtil.addClassNameRecursively(elem, fakeFocusClass);
+        documentUtil.rewriteCssSelectorWith(doc, ':focus', '.' + fakeFocusClass);
+    };
+
+    module.persistInputValues = function (doc) {
+        var inputs = doc.querySelectorAll('input'),
+            textareas = doc.querySelectorAll('textarea'),
+            isCheckable = function (input) {
+                return input.type === 'checkbox' || input.type === 'radio';
+            };
+
+        asArray(inputs).filter(isCheckable)
+            .forEach(function (input) {
+                if (input.checked) {
+                    input.setAttribute('checked', '');
+                } else {
+                    input.removeAttribute('checked');
+                }
+            });
+
+        asArray(inputs).filter(function (input) { return !isCheckable(input); })
+            .forEach(function (input) {
+                input.setAttribute('value', input.value);
+            });
+
+        asArray(textareas)
+            .forEach(function (textarea) {
+                textarea.textContent = textarea.value;
+            });
+    };
+
+    module.rewriteTagNameSelectorsToLowerCase = function (doc) {
+        documentUtil.lowercaseCssTypeSelectors(doc, documentUtil.findHtmlOnlyNodeNames(doc));
+    };
+
+    return module;
+}(documentUtil));
